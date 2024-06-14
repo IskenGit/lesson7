157c94426e85aec788bc85177a8198dcd4706f1e
IskenGit <isken1990@gmail.com>
2024-06-14 10:24:29 +0600

    change code

---
+++ main.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+class Weapon(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+class Sword(Weapon):
+    def attack(self):
+        print(f'Боец наносит удар мечом.')
+
+class Bow(Weapon):
+    def attack(self):
+        print("Боец наносит удар из лука.")
+class Fighter:
+    def __init__(self, name):
+        self.name = name
+        self.weapon = None
+
+    def changeWeapon(self, weapon: Weapon):
+        self.weapon = weapon
+
+    def attack(self):
+        if self.weapon:
+            print(f"{self.name} ", end="")
+            self.weapon.attack()
+        else:
+            print("У бойца нет оружия.")
+class Monster:
+    def __init__(self, name):
+        self.name = name
+
+    def defeat(self):
+        print(f"Монстр {self.name} побежден!")
+
+def battle(fighter: Fighter, monster: Monster):
+    fighter.attack()
+    monster.defeat()
+
+# Демонстрация работы
+fighter = Fighter("Рыцарь")
+monster = Monster("Гоблин")
+
+# Боец выбирает меч
+fighter.changeWeapon(Sword())
+print(f"{fighter.name} выбирает меч.")
+battle(fighter, monster)
+
+# Боец выбирает лук
+fighter.changeWeapon(Bow())
+print(f"\n{fighter.name} выбирает лук.")
+battle(fighter, monster)
